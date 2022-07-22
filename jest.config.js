d07af26d32fe23ccb6cdbe64e161a8d0714bad56
Dominic Arrojado <dominicarrojado@gmail.com>
2022-07-22 14:34:38 +0800

    clean up code

--- jest.config.js
+++ jest.config.js
@@ -8,12 +8,7 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you soon)
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-
-    '^@/pages/(.*)$': '<rootDir>/pages/$1',
-  },
+  moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
 };
 
